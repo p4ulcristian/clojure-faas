7765051dbde6970b6742531021b827da85fea88f
Paul Cristian <paul931224@gmail.com>
2020-03-07 15:17:47 +0100

    http:service-name?

--- openclojure/src/clj/function/handler.clj
+++ openclojure/src/clj/function/handler.clj
@@ -29,7 +29,7 @@
          [:h3 "lets see if it stays unmodified."]
          [:h4 (str "Hello There successful :)")]
          [:h4 "First api endpoint: "
-          (str  (client/get "paul931224-rand-name"))]]))
+          (str  (client/get "http://paul931224-rand-name"))]]))
          ;(include-js "js/app.js")]))
 
 (defn request-wrap [status content-type body]
